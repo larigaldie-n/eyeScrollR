a40cc8f36b9053d4d8823ddacff9a80d975dbe18
Larigaldie Nathanael <larigaldie.n@gmail.com>
2023-01-19 15:23:08 +0100

    Added (temporary) citation, refactored code, removed inconsistencies

--- tests/testthat/test-fixed_areas_bundle.R
+++ tests/testthat/test-fixed_areas_bundle.R
@@ -1,14 +1,43 @@
 test_that("Properly returns error if no arguments or non-paired arguments", {
-  expect_error(fixed_areas_bundle(), "You must pass screen-to-image mapping pairs of coordinate vectors to this function")
-  expect_error(fixed_areas_bundle(c(0,0,1,2)), "You must pass screen-to-image mapping pairs of coordinate vectors to this function")
+  expect_error(
+    fixed_areas_bundle(),
+    "You must pass screen-to-image mapping pairs of coordinate vectors to this function"
+  )
+  expect_error(
+    fixed_areas_bundle(c(0, 0, 1, 2)),
+    "You must pass screen-to-image mapping pairs of coordinate vectors to this function"
+  )
 })
 
-test_that("Properly returns error if arguments are not vectors, or are not of length 4", {
-  expect_error(fixed_areas_bundle(1,2), "Every argument should be a coordinate vector in the form: c(top_left_x, top_left_y, bottom_right_x, bottom_right_y)", fixed = TRUE)
-  expect_error(fixed_areas_bundle(c(0,0,1), c(5,3,6,5)), "Every argument should be a coordinate vector in the form: c(top_left_x, top_left_y, bottom_right_x, bottom_right_y)", fixed = TRUE)
-})
+test_that("Properly returns error if arguments are not vectors, or are not of length 4",
+          {
+            expect_error(
+              fixed_areas_bundle(1, 2),
+              "Every argument should be a coordinate vector in the form: c(top_left_x, top_left_y, bottom_right_x, bottom_right_y)",
+              fixed = TRUE
+            )
+            expect_error(
+              fixed_areas_bundle(c(0, 0, 1), c(5, 3, 6, 5)),
+              "Every argument should be a coordinate vector in the form: c(top_left_x, top_left_y, bottom_right_x, bottom_right_y)",
+              fixed = TRUE
+            )
+          })
 
 test_that("Properly returns an array of fixed areas bundle", {
-  expect_equal(fixed_areas_bundle(c(0,0,1,2), c(0,0,3,4)), array(c(c(0,0,1,2), c(0,0,3,4)), dim = c(4,2,1)))
-  expect_equal(fixed_areas_bundle(c(0,0,1,2), c(0,0,3,4), c(5,6,7,8), c(8,5,6,8), c(10,11,12,13), c(14,15,16,17)), array(c(c(0,0,1,2), c(0,0,3,4), c(5,6,7,8), c(8,5,6,8), c(10,11,12,13), c(14,15,16,17)), dim = c(4,2,3)))
+  expect_equal(fixed_areas_bundle(c(0, 0, 1, 2), c(0, 0, 3, 4)), array(c(c(0, 0, 1, 2), c(0, 0, 3, 4)), dim = c(4, 2, 1)))
+  expect_equal(fixed_areas_bundle(
+    c(0, 0, 1, 2),
+    c(0, 0, 3, 4),
+    c(5, 6, 7, 8),
+    c(8, 5, 6, 8),
+    c(10, 11, 12, 13),
+    c(14, 15, 16, 17)
+  ), array(c(
+    c(0, 0, 1, 2),
+    c(0, 0, 3, 4),
+    c(5, 6, 7, 8),
+    c(8, 5, 6, 8),
+    c(10, 11, 12, 13),
+    c(14, 15, 16, 17)
+  ), dim = c(4, 2, 3)))
 })
--- tests/testthat/test-shift_image_by_dimension.R
+++ tests/testthat/test-shift_image_by_dimension.R
@@ -6,11 +6,29 @@ test_that("Properly shifts a point inside the image", {
 
   # With outside values discarded
   outside_image_is_na <- TRUE
-  expect_equal(shift_image_by_dimension(coordinate, shift_before, shift_after, screen_dimension, outside_image_is_na), 75)
+  expect_equal(
+    shift_image_by_dimension(
+      coordinate,
+      shift_before,
+      shift_after,
+      screen_dimension,
+      outside_image_is_na
+    ),
+    75
+  )
 
   # With outside values kept
   outside_image_is_na <- FALSE
-  expect_equal(shift_image_by_dimension(coordinate, shift_before, shift_after, screen_dimension, outside_image_is_na), 75)
+  expect_equal(
+    shift_image_by_dimension(
+      coordinate,
+      shift_before,
+      shift_after,
+      screen_dimension,
+      outside_image_is_na
+    ),
+    75
+  )
 })
 
 test_that("Properly handles a point before the image", {
@@ -21,11 +39,29 @@ test_that("Properly handles a point before the image", {
 
   # Points discarded outside the image
   outside_image_is_na <- TRUE
-  expect_equal(shift_image_by_dimension(coordinate, shift_before, shift_after, screen_dimensio, outside_image_is_na), NA)
+  expect_equal(
+    shift_image_by_dimension(
+      coordinate,
+      shift_before,
+      shift_after,
+      screen_dimensio,
+      outside_image_is_na
+    ),
+    NA
+  )
 
   # Points kept outside the image
   outside_image_is_na <- FALSE
-  expect_equal(shift_image_by_dimension(coordinate, shift_before, shift_after, screen_dimensio, outside_image_is_na), 10-25)
+  expect_equal(
+    shift_image_by_dimension(
+      coordinate,
+      shift_before,
+      shift_after,
+      screen_dimensio,
+      outside_image_is_na
+    ),
+    10 - 25
+  )
 })
 
 test_that("Properly handles a point after the image", {
@@ -36,27 +72,64 @@ test_that("Properly handles a point after the image", {
 
   # Points discarded outside the image
   outside_image_is_na <- TRUE
-  expect_equal(shift_image_by_dimension(coordinate, shift_before, shift_after, screen_dimension, outside_image_is_na), NA)
+  expect_equal(
+    shift_image_by_dimension(
+      coordinate,
+      shift_before,
+      shift_after,
+      screen_dimension,
+      outside_image_is_na
+    ),
+    NA
+  )
 
   # Points kept outside the image
   outside_image_is_na <- FALSE
-  expect_equal(shift_image_by_dimension(coordinate, shift_before, shift_after, screen_dimensio, outside_image_is_na), 1100-25)
+  expect_equal(
+    shift_image_by_dimension(
+      coordinate,
+      shift_before,
+      shift_after,
+      screen_dimensio,
+      outside_image_is_na
+    ),
+    1100 - 25
+  )
 })
 
-test_that("Properly handles a point right after the image, when there is a small shift before", {
-  coordinate <- 985
-  shift_before <- 25
-  shift_after <- 25
-  screen_dimension <- 1000
+test_that("Properly handles a point right after the image, when there is a small shift before",
+          {
+            coordinate <- 985
+            shift_before <- 25
+            shift_after <- 25
+            screen_dimension <- 1000
 
-  # Points discarded outside the image
-  outside_image_is_na <- TRUE
-  expect_equal(shift_image_by_dimension(coordinate, shift_before, shift_after, screen_dimension, outside_image_is_na), NA)
+            # Points discarded outside the image
+            outside_image_is_na <- TRUE
+            expect_equal(
+              shift_image_by_dimension(
+                coordinate,
+                shift_before,
+                shift_after,
+                screen_dimension,
+                outside_image_is_na
+              ),
+              NA
+            )
 
-  # Points kept outside the image
-  outside_image_is_na <- FALSE
-  expect_equal(shift_image_by_dimension(coordinate, shift_before, shift_after, screen_dimensio, outside_image_is_na), 985-25)
-})
+            # Points kept outside the image
+            outside_image_is_na <- FALSE
+            expect_equal(
+              shift_image_by_dimension(
+                coordinate,
+                shift_before,
+                shift_after,
+                screen_dimensio,
+                outside_image_is_na
+              ),
+              985 - 25
+            )
+          })
 
 test_that("Properly handles an NA coordinate", {
   coordinate <- NA
@@ -66,9 +139,27 @@ test_that("Properly handles an NA coordinate", {
 
   # Points discarded outside the image
   outside_image_is_na <- TRUE
-  expect_equal(shift_image_by_dimension(coordinate, shift_before, shift_after, screen_dimension, outside_image_is_na), NA)
+  expect_equal(
+    shift_image_by_dimension(
+      coordinate,
+      shift_before,
+      shift_after,
+      screen_dimension,
+      outside_image_is_na
+    ),
+    NA
+  )
 
   # Points kept outside the image
   outside_image_is_na <- FALSE
-  expect_equal(shift_image_by_dimension(coordinate, shift_before, shift_after, screen_dimensio, outside_image_is_na), NA)
+  expect_equal(
+    shift_image_by_dimension(
+      coordinate,
+      shift_before,
+      shift_after,
+      screen_dimensio,
+      outside_image_is_na
+    ),
+    NA
+  )
 })
